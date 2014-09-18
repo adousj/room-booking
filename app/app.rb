1d0652ca8e60688349aace2a1ae3e63ecc7e8e18
adousj <adousj@gmail.com>
2014-09-18 14:12:39 +0800

    增加 home_controller，将首页相关mehtod放进去

--- app/app.rb
+++ app/app.rb
@@ -86,52 +86,7 @@ module RoomBooking
     end
 
     get :index do
-      redirect url(:day, :date => Time.now.to_date)
-    end
-
-    get :day, :with => :date do
-      @show_bottom_imgs = true
-      mod_num = 100
-      if params[:date].nil? or not params[:date].respond_to?(:to_date)
-        @current_date = Time.now.to_date
-      else
-        @current_date = params[:date].to_date
-      end
-      unless @current_date
-        flash[:error] = '对不起，请选择有效申请时间~'
-        redirect url(:day, :date => Time.now.to_date)
-      end
-      @start_date = (@current_account and @current_account.is_admin?) ? Time.new(0).to_date : Time.now.to_date
-      @end_date = Chronic.parse('7 days after now').to_date
-      if @start_date<=@current_date and @current_date<=@end_date
-        @time_line = @current_date==Time.now.to_date ? ((Time.now-15*60).getlocal.hour+1)*mod_num : -1
-        today_start_time = Chronic.parse '9:0', :now => @current_date
-        today_end_time = Chronic.parse '17:00', :now => @current_date
-        apps = Application.find(:all, :conditions => ['start_at>=? and end_at<=? and status=?', today_start_time, today_end_time, Application.statuses[:approved]])
-        @apps = apps.map { |app|
-          (app.start_at.getlocal.hour...app.end_at.getlocal.hour).map {|h| h*mod_num+app.room_id}
-        }.inject(&:+) || []
-        render 'index'
-      end
-    end
-
-    get :rules do
-      @post = Post.first
-      render 'post'
-    end
-
-    get :guides do
-      @post = Post.offset(1).first
-      render 'post'
-    end
-
-    get :contact_us do
-      @post = Post.offset(2).first
-      render 'post'
-    end
-
-    get :blacklist do
-      render 'black_list'
+      redirect url(:home, :day, :date => Time.now.to_date)
     end
 
     ##
